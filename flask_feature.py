f38d6defc2bb3de6a2b6904422bc261b1ce46c77
Brian O'Connor <gatzby3jr@gmail.com>
2014-07-02 14:51:50 -0400

    Memoize checks on a per-request / app context, get rid of useless get_value function, and change var name

--- flask_feature.py
+++ flask_feature.py
@@ -5,6 +5,8 @@ __license__ = 'MIT'
 __copyright__ = "(c) 2014 by Brian O'Connor"
 __all__ = ['FeatureManager']
 
+from flask import current_app, _app_ctx_stack as stack
+
 from flask.ext.login import current_user
 
 class FeatureManager(object):
@@ -14,16 +16,31 @@ class FeatureManager(object):
             self.init_app(app)
 
     def init_app(self, app):
-        self.flags = app.config.get('FEATURES', {})
+        self.data = app.config.get('FEATURES', {})
         app.feature = self
 
     def is_enabled(self, key):
-        value = self.get_value(key)
+        ctx = stack.top
+
+        if ctx is not None:
+            if not hasattr(ctx, 'memoized_feature_flags'):
+                ctx.memoized_feature_flags = {}
+
+            if key in ctx.memoized_feature_flags:
+                current_app.logger.debug("Found {0} in memoized app context".format(key))
+                return ctx.memoized_feature_flags[key]
+
+        current_app.logger.debug("Checking if {0} is enabled in context".format(key))
+
+        value = self.data.get(key, False)
 
         if value == 'admin':
-            return ((current_user is not None) and current_user.is_authenticated() and current_user.is_admin())
+            value = ((current_user is not None) and current_user.is_authenticated() and current_user.is_admin())
 
-        return bool(value)
+        value = bool(value)
 
-    def get_value(self, key):
-        return self.flags.get(key, False)
+        # Memoize this in the app context
+        ctx.memoized_feature_flags[key] = value
+        current_app.logger.debug("Set memoized value for {0} to {1}".format(key, value))
+
+        return value
